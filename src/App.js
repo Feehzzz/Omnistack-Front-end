e59f3e78c1014b2b494c19486cc007bcc13a5c61
Feehzzz <fehshuffle@gmail.com>
2019-04-11 22:51:13 -0300

    frontend finalizado com atualização realtime

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Main from './pages/Main';
+import Routes from './routes';
 
+const App = () => <Routes/>
 
-class App extends Component {
-  render() {
-    return <Main/>
-  }
-}
 
 export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseUrl:'https://omniweekdrop.herokuapp.com',
-
+    baseURL:'https://omniweekdrop.herokuapp.com' ,
 });
 
 export default api;
